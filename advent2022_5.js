d55a6516d31d2bbbe3a8074eaa4d4edee3008b38
TeddyBeer94 <achenisbest@free.fr>
2022-12-05 15:13:09 +0100

    Changement de certaines fonction, utilisation de reduce

--- advent2022_5.js
+++ advent2022_5.js
@@ -10,53 +10,53 @@ const moves = array[1].split('\n').filter(x =>x).map((v,i) =>({
 const rowstack = array[0].split('\n').map((v,i) => v.split('    ').map(x=>x.split(' ')).join().split(','))
 const nbstacks = rowstack[0].length
 const zeros = Array.from({length: nbstacks}, (v,i) => 0)
-var col_stack = zeros.map((v,i) => rowstack.map((w,j) => {
-    if (w[i] != '')  return w[i]})).map((v,i) => v.filter((w,j) => j+1< v.length)).map((v,i) => v.filter(x => x))
+var col_stack = zeros.map((v,i) => rowstack.map((w,j) =>  w[i]))
+    .map((v,i) => v.filter((w,j) => j+1< v.length))
+    .map((v,i) => v.filter(x => x!= ''))
 
 const rev = ((tab) => tab.map((v,i) => tab[tab.length -1 -i]))
 
-const move_n = (({nbmoves,from,to}) => {
-    if (nbmoves >= col_stack[from -1]) {
-        const moved = col_stack[from -1]
-        var x = rev(col_stack[to-1])
-        col_stack[to-1] = rev(x.concat(moved))
-        col_stack[from-1] = []
+const move_n = (({tab,nbmoves,from,to}) => {
+    if (nbmoves >= tab[from -1].length) {
+        const moved = tab[from -1]
+        var x = rev(tab[to-1])
+        tab[to-1] = rev(x.concat(moved))
+        tab[from-1] = []
     }
     else {
-        const moved = col_stack[from -1].slice(0,nbmoves)
-        col_stack[from-1] = col_stack[from -1].slice(nbmoves)
-        var x = rev(col_stack[to-1])
-        col_stack[to-1] = rev(x.concat(moved))
+        const moved = tab[from -1].slice(0,nbmoves)
+        tab[from-1] = tab[from -1].slice(nbmoves)
+        var x = rev(tab[to-1])
+        tab[to-1] = rev(x.concat(moved))
     }
-
+    return tab
 })
 
-const allmoves = moves.map((v,i) => {
-    move_n(v)
-})
+const allmoves = moves.reduce((acc,cur) => {
+    return move_n({tab : acc , nbmoves : cur.nbmoves , from : cur.from , to : cur.to})
+}, col_stack)
 
-console.log(col_stack.map((v,i) => v[0]))
+console.log(allmoves.map((v,i) => v[0]))
 
-var col_stack = zeros.map((v,i) => rowstack.map((w,j) => {
-    if (w[i] != '')  return w[i]})).map((v,i) => v.filter((w,j) => j+1< v.length)).map((v,i) => v.filter(x => x))
-
-const move_n2 = (({nbmoves,from,to}) => {
-    if (nbmoves >= col_stack[from -1]) {
-        const moved = col_stack[from -1]
-        col_stack[to-1] = moved.concat(col_stack[to-1])
-        col_stack[from-1] = []
+    
+const move_n2 = (({tab,nbmoves,from,to}) => {
+    if (nbmoves >= tab[from -1]) {
+        const moved = tab[from -1]
+        tab[to-1] = moved.concat(tab[to-1])
+        tab[from-1] = []
       }
     else {
-        const moved = col_stack[from -1].slice(0,nbmoves)
-        col_stack[from-1] = col_stack[from -1].slice(nbmoves)
-        col_stack[to-1] = moved.concat(col_stack[to-1])
+        const moved = tab[from -1].slice(0,nbmoves)
+        tab[from-1] = tab[from -1].slice(nbmoves)
+        tab[to-1] = moved.concat(tab[to-1])
     }
-
+    return tab
 })
 
-const allmoves2 = moves.map((v,i) => {
-    move_n2(v)
-})
+const allmoves2 = moves.reduce((acc,cur) => {
+    return move_n2({tab : acc , nbmoves : cur.nbmoves , from : cur.from , to : cur.to})
+}, col_stack)
 
-console.log(col_stack.map((v,i) => v[0]))
+
+console.log(allmoves2.map((v,i) => v[0]))
 console.timeEnd('\nExecution time')
