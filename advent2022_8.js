109e4e16c360f83f4ddbd2b6a9b5d074a4a00a51
TeddyBeer94 <achenisbest@free.fr>
2022-12-08 10:53:53 +0100

    modification du jour 8 , temps d'execution ~14 ms

--- advent2022_8.js
+++ advent2022_8.js
@@ -3,25 +3,22 @@ console.time('\nExecution time')
 const array = fs.readFileSync('adv2022_8_file', 'utf8').trim().split('\n');
 var lines = array.map((v,i) => v.split('').map((w,j) => ({height:Number(w) , seen : false,score :1})))
 
-const rev = ((tab) => tab.map((v,i) => tab[tab.length -1 -i]))
 
 const getcol = ((indexcol) => lines.map((v,i) => v[indexcol]))
 
-const operation = ((tab) => tab.reduce((acc,cur,index)=> {
-    if (cur.height > acc.height || index ==0) {cur.seen = true , acc.height = cur.height ,cur.score = cur.score * index}
-    else {
-        cur.score = cur.score * count_sight(cur.height,acc.line).tot
-    }
-    acc.line.push(cur)
+const operation = ((tab) => tab.reduce((acc,cur)=> {
+    if (cur.height > acc.height ) {cur.seen = true , acc.height = cur.height}
+    cur.score = cur.score * count_sight(cur.height,acc.line).tot
+    acc.line.push(cur.height)
     return acc
     }
-, {height : 0 , line : []}
+, {height : -1 , line : []}
 ))
 
-const count_sight = ((height , line) => rev(line).reduce((acc,cur) => {
+const count_sight = ((height , line) => line.reverse().reduce((acc,cur) => {
     if (acc.stop == false) {
         {acc.tot = acc.tot +1}
-        if (height <= cur.height) {acc.stop = true} 
+        if (height <= cur) {acc.stop = true} 
     }
     return acc
 }, {tot : 0, stop : false}))
@@ -29,11 +26,11 @@ const count_sight = ((height , line) => rev(line).reduce((acc,cur) => {
 
 const testleft = ((indexl)=> operation(lines[indexl]))
 
-const testright = ((indexl)=> operation(rev(lines[indexl])))
+const testright = ((indexl)=> operation(lines[indexl].reverse()))
 
 const testtop = ((indexcol) => operation(getcol(indexcol)))
 
-const testbot = ((indexcol) => operation(rev(getcol(indexcol))))
+const testbot = ((indexcol) => operation(getcol(indexcol).reverse()))
 
 lines.map((v,i) => {
     testleft(i)
