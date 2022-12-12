a502c39eae14593e4d123b89fbb88ce5849e229b
TeddyBeer94 <achenisbest@free.fr>
2022-12-12 13:17:59 +0100

    modification jour 12, temps d'execution ~25 ms

--- advent2022_12.js
+++ advent2022_12.js
@@ -7,9 +7,9 @@ var cases = array.map((v,i) => v.split('').map((w,j) => ({
     position : {x : i , y : j},
     dist_to_S : 1000000
 })))
+const init = JSON.parse(JSON.stringify(cases))
 
 const rev = ((tab) => tab.map((v,i) => tab[tab.length -1 -i]))
-
 const getcol = ((indexcol) => cases.map((v,i) => v[indexcol]))
 
 const height = ((char) => {
@@ -19,72 +19,70 @@ const height = ((char) => {
         return char.charCodeAt() -96
     }
 })
-const operation = ((tab) => tab.reduce((acc,cur) => {
+const height2 = ((char) => 26-height(char))
+
+const operation = ((tab,heightf) => tab.reduce((acc,cur) => {
     if (acc == 0) {}
     else{
-        if (height(acc.letter)-height(cur.letter) <=1) {
+        if (heightf(acc.letter)-heightf(cur.letter) <=1) {
             cur.neighbours.push(acc.position)
         }
     }
     return {position : cur.position , letter : cur.letter}
 } , 0))
 
-cases.map((v,i) => {
-    operation(cases[i])
-    operation(rev(cases[i]))})
 
-cases[0].map((v,i) => {
-    operation(getcol(i))
-    operation(rev(getcol(i)))})
+const format = ((heightf) => {
+    cases.map((v,i) => {
+        operation(cases[i],heightf)
+        operation(rev(cases[i]),heightf)})
 
-const copy_cases = JSON.parse(JSON.stringify(cases))
+    cases[0].map((v,i) => {
+        operation(getcol(i),heightf)
+        operation(rev(getcol(i)),heightf)})
+}) 
+
+format(height)
+const cases_up = JSON.parse(JSON.stringify(cases))
+cases = init
+format(height2)
+const cases_down = JSON.parse(JSON.stringify(cases))
 
 const change_dist = ((tile) => tile.neighbours.map((v,i) => {
     if (cases[v.x][v.y].dist_to_S > tile.dist_to_S +1) {
         cases[v.x][v.y].dist_to_S = tile.dist_to_S +1
-        to_visit.add(cases[v.x][v.y])
+        to_visit.push(cases[v.x][v.y])
     }
-    to_visit.delete(tile)
 }
 ))
 
-const getmin = (() => Array.from(to_visit).reduce((acc,cur) => {
-    if (acc.dist_to_S > cur.dist_to_S) {
-        return cur
-    }
-    return acc
-}, {dist_to_S : Infinity}))
-
 const posS = cases.map(x => (x.filter(x => x.letter == 'S'))).filter(x => x.length >0)[0][0].position
-const posE = cases.map(x => (x.filter(x => x.letter == 'E'))).filter(x => x.length >0)[0][0].position
 
-var to_visit = new Set()
+var to_visit = []
 
-const dist_to_E = (({xstart,ystart}) => {
-    cases = copy_cases
-    to_visit.add(cases[xstart][ystart])
-    cases[xstart][ystart].dist_to_S = 0
-    while (to_visit.size > 0) {
-        const min_pos = getmin().position
-        change_dist(cases[min_pos.x][min_pos.y])
+const dist_to_E = (({xstart,ystart,charend,up}) => {
+    if (up) {
+        cases = cases_up
     }
-    return cases[posE.x][posE.y].dist_to_S
+    else {
+        cases = cases_down
+    }
+    to_visit.splice(0,to_visit.length)
+    to_visit.push(cases[xstart][ystart])
+    cases[xstart][ystart].dist_to_S = 0
+    while (to_visit.length > 0) {
+        const min_pos = to_visit.shift().position
+        change_dist(cases[min_pos.x][min_pos.y])
+        if (cases[min_pos.x][min_pos.y].letter == charend) {
+            return cases[min_pos.x][min_pos.y]
+        }
+        }
 })
 
-const result1 = dist_to_E({xstart : posS.x , ystart : posS.y})
+const result1 = dist_to_E({xstart : posS.x , ystart : posS.y,charend : 'E',up : true})
 
-var SET_a = new Set()
-
-cases.map((v) => v.map((w) => {
-    if (w.letter == 'a') {SET_a.add(w)}
-}))
-
-const array_dist = Array.from(SET_a).map((v) => 
-dist_to_E({xstart : v.position.x, ystart : v.position.y})
-)
-
-const result2 = Math.min(...array_dist)
+const result2 = dist_to_E({xstart : result1.position.x , ystart : result1.position.y,charend : 'a', up : false}).dist_to_S
 
 console.timeEnd('\nExecution time')
-console.log(result1)
+console.log(result1.dist_to_S)
 console.log(result2)
