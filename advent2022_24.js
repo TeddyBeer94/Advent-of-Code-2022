dcbfa25ba6a18fb978820ae1f27c4ebb6128d92e
TeddyBeer94 <achenisbest@free.fr>
2022-12-24 10:14:06 +0100

    ajout jour 24, temps d'execution ~ 26 s

--- advent2022_24.js
+++ advent2022_24.js
@@ -1,8 +1,7 @@
 import fs from 'fs';
 console.time('\nExecution time')
-//const array = fs.readFileSync('example24','utf8').trim().split('\r\n')
 const array = fs.readFileSync('adv2022_24_file', 'utf8').trim().split('\n');
-const map = array.map((v,i) => v.split(''))
+const map = array.map((v) => v.split(''))
 
 const to_key = (({x,y}) => x.toString()+','+y.toString())
 const layout = new Map()
@@ -12,56 +11,40 @@ const width = map[0].length
 const start = {x : 1, y : 0}
 const end = {x : width -2,y : h -1 }
 
-
 map.map((v,y) => v.map((w,x) => {
     if (['>','<','^','v'].includes(w)) {blizzards.push({x : x , y : y, dir : w})}
     if (w != '.') {layout.set(to_key({x,y}),w)}
 }))
 
 //to prevent the player from esacping the maze from the top or bottom
-layout.set(to_key({x : 1,y : -1}),'#')
+layout.set(to_key({x : start.x ,y : start.y-1}),'#')
 layout.set(to_key({x : end.x , y : end.x +1}),'#')
 
-
-const disp = (() => {
-    let image = ''
-    for (let y = 0; y <=h-1 ; y++) {
-        for (let x = 0 ; x <=width -1 ; x++) {
-            let key = to_key({x,y})
-            if(typeof(layout.get(key)) == 'undefined') {image += '.'}
-            else {image += layout.get(key)}
-        }
-        image += '\n'
-    }
-    console.log(image)
-})
-
+//returns a blizzard's next position
 const move_blizz = ((blizzard) => {
     if (blizzard.dir == '>') {let next = {x : blizzard.x +1 , y : blizzard.y , dir : blizzard.dir} 
         if (next.x >= width-1) {next.x = 1}
-        return next
-    }
+        return next}
     if (blizzard.dir == '^') {let next = {x : blizzard.x , y : blizzard.y -1, dir : blizzard.dir} 
-    if (next.y == start.y && next.x != start.x) {next.y = h-2}
-    return next
-}
+        if (next.y == start.y && next.x != start.x) {next.y = h-2}
+        return next}
     if (blizzard.dir == '<') {let next = {x : blizzard.x -1 , y : blizzard.y , dir : blizzard.dir} 
-    if (next.x == 0) {next.x = width -2}
-    return next}
+        if (next.x == 0) {next.x = width -2}
+        return next}
     if (blizzard.dir == 'v') {let next = {x : blizzard.x , y : blizzard.y +1, dir : blizzard.dir} 
-    if (next.y == end.y && next.x != end.x) {next.y = 1}
-    return next}
+        if (next.y == end.y && next.x != end.x) {next.y = 1}
+        return next}
 })
 
 const update_layout = (() => {
     let next_blizz = blizzards.map((v)=> {
-        layout.delete(to_key(v))
-        return move_blizz(v)
+        layout.delete(to_key(v)) ; return move_blizz(v)
     })
     next_blizz.map((v) => layout.set(to_key(v),v.dir))
     blizzards = next_blizz
 })
 
+//return new possible positions at minute n+1 given a position at minute n 
 const new_states = ((position) => {
     let next_positions = [position , {x : position.x , y : position.y +1},
     {x : position.x , y : position.y -1},{x : position.x +1, y : position.y},
@@ -69,14 +52,14 @@ const new_states = ((position) => {
     return next_positions.filter(x => typeof(layout.get(to_key(x))) == 'undefined')
 })
 
+//return all possible positions at minute n+1 given all possible positions at minute n
 const all_new_positions = ((positions) => positions.reduce((acc,cur) => {
     let next = new_states(cur)
-    next.map((v)=> {
-        acc.add(to_key(v))
-    })
+    next.map((v)=> acc.add(to_key(v)))
     return acc
 }, new Set()))
 
+// moves the blizzards and returns new possible positions
 const minute = ((positions) => {
     update_layout()
     return Array.from(all_new_positions(positions)).map((v) => 
@@ -84,28 +67,24 @@ const minute = ((positions) => {
 })
 
 const check_end = ((positions,end) => positions.reduce((acc,cur) => {
-    if (cur.x === end.x && cur.y === end.y) {
-        return true
-    }
+    if (cur.x === end.x && cur.y === end.y) {return true}
     return acc
 },false))
 
-const iterate = ((start,end) => {
-    let i = 0
-    let positions = [start]
+const journey = ((start,end) => {
+    let i = 0 ; let positions = [start]
     while (check_end(positions,end)== false) {
         positions = minute(positions)
         i +=1   
     }
     return i
 })
-const result1 = iterate(start,end)
-const length_trip_back = iterate(end, start)
-const length_return = iterate(start,end)
 
+const result1 = journey(start,end)
+const length_trip_back = journey(end, start)
+const length_return = journey(start,end)
 const result2 = result1 + length_trip_back + length_return
 
 console.timeEnd('\nExecution time')
-
 console.log(result1)
 console.log(result2)
